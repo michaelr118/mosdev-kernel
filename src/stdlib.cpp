16c9090a762977db21346bfda8b5a984211c9408
michaelr <michael.jroberts118@gmail.com>
2018-02-02 00:06:38 -0700

    added kernel panic. yay

--- src/stdlib.cpp
+++ src/stdlib.cpp
@@ -1,22 +1,16 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+/*
+ * Called when there is a kernel panic
+*/
 __attribute__((__noreturn__))
 void abort(void)
 {
-#if defined(__is_libk)
 	printf("Kernel panic: abort()\n");
-#else
-	printf("abort()\n");
-#endif
+
+	// TODO: Add debug information for kernel panic
+	
 	while (1) { }
 	__builtin_unreachable();
 }
-
-#ifdef __cplusplus
-}
-#endif
