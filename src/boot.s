9a4e10ea52e023444622f9ac418b5d22d8a384ca
michaelr <michael.jroberts118@gmail.com>
2018-02-03 17:47:59 -0700

    added basic GDT

--- src/boot.s
+++ src/boot.s
@@ -6,6 +6,7 @@
 .set CHECKSUM, -(MAGIC + OPT)
 
 .section .multiboot
+
 .align 4
 .long MAGIC
 .long OPT
@@ -14,12 +15,39 @@
 /* END MULTIBOOT HEADER */
 
 .section .bss
+
 .align 16
 stack_bottom:
 .skip 16384
 stack_top:
 
 .section .text
+
+.extern _pgdt
+
+.global _gdt_set
+.type _gdt_set, @function
+
+_gdt_set:
+	lgdt (_pgdt)
+	mov $0x10, %ax
+	mov %ax, %ds
+	mov %ax, %es
+	mov %ax, %fs
+	mov %ax, %gs
+	mov %ax, %ss
+	jmp $0x08, $_gdt_set_ret
+
+_gdt_set_ret:
+	ret
+
+.global _tss_flush
+.type _tss_flush, @function
+
+_tss_flush:
+	mov $0x2b, %ax
+	ltr %ax
+	ret
 
 .global _boot_main
 .type _boot_main, @function
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "gdt.h"
 #include "ioaccess.inl"
 #include "stdio.h"
 #include "tty.h"
 
 extern "C" void kernel_main(void)
 {
+	// Install our Global Descriptor Table (GDT)
+	gdt_install();
+
 	tty_init();
 	printf("Welcome. vmmos-i386\n\nKernel says...\n\n");
 }
