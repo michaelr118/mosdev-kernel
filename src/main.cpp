2aa47d984f103023ed3e2222e45a3138f2c35498
michaelr <michael.jroberts118@gmail.com>
2018-02-03 18:18:35 -0700

    disabled interrupts while installing GDT

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@
 extern "C" void kernel_main(void)
 {
 	// Install our Global Descriptor Table (GDT)
+	// Interrupts should be disabled for this. Re-enabled when done
+	__asm__("cli");
 	gdt_install();
+	__asm__("sti");
 
 	tty_init();
 	printf("Welcome. vmmos-i386\n\nKernel says...\n\n");
